c1bb4012f387b911f8eda727386f96d7c33d7a61
Pep Mendez <jmendez1@xtec.cat>
2020-10-20 16:08:16 +0200

    Added class M03_UF04_SecondAssignment

---
+++ src/main/java/cat/copernic/m03_uf04_secondassignment/M03_UF04_SecondAssignment.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.m03_uf04_secondassignment;
+
+/**
+ *
+ * @author pep
+ */
+public class M03_UF04_SecondAssignment {
+    
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+    
+}
